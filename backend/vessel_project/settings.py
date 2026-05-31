2943d319decadcc7ee43d2fcdb26e70e37249604
Prasen Nimje <144055291+Prasen8@users.noreply.github.com>
2026-06-01 01:38:43 +0530

    setting.py

--- backend/vessel_project/settings.py
+++ backend/vessel_project/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://vessle-market-analysis.onrender.com/api",
+    "https://vessle-market-analysis-czvz.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
